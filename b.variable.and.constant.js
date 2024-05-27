a49c53b0b59dfa07d521dc248d5e70702b46f20f
Prashant Acharya <109913165+theblitheguy@users.noreply.github.com>
2024-05-27 20:46:27 +0545

    Add files via upload

---
+++ b.variable.and.constant.js
@@ -0,0 +1,42 @@
+// ? variable 
+
+//syntax
+//let varianleName =value
+
+let studentName = 'Prashant Acharya';
+console.log(studentName);
+
+// ? namning convention
+// ? 1. variable name cannot be reserved keyword
+// ? such as for,let,const. while, if, switch
+// ? 2. variable name cannot start with number
+// ? 3. variable name cannot contain special charater
+// ? except _ and $
+
+let _ = 5;
+let $ =2 ;
+console.log(_);
+console.log($);
+
+let first_name = Ram
+
+// ? 4. variable name should be descriptive
+// ? 5. variable name should be camel case
+// ? upper case => NEPAL
+// ? lower case => nepal
+
+let firstName = 'A';
+let isGraduated = false;
+
+// ? constant
+
+// const x = 7;
+// x =10;   // mildaina kina ki pailai assing bhaisako
+// console.log(x);
+
+// universal constant are written in uppercase
+const PI = Math.PI;
+console.log(PI);
+
+
+
