3d47b9ba7424e1adeba3c9d1fe4afca33edce9ed
Prashant Acharya <109913165+theblitheguy@users.noreply.github.com>
2024-06-04 21:50:20 +0545

    Add files via upload

---
+++ Day_2/e.type.conversion.js
@@ -0,0 +1,28 @@
+// ? string to number conversion
+
+const x ="45";  // numeric string
+
+// const y = Number(x);  // const use nagarda var equivalent hunxa 
+// const y =+x;  //yesma "+" garda number hunxa mathiko jasto
+// console.log(y, typeof y);
+
+// ? parseInt, parseFloat
+
+//parseInt le point paxi ko dekhaudaina
+// const m = '11.25';
+// const n = parseInt(m);
+// console.log(n, typeof n);
+
+// ? to string conversion
+
+// const  isMarried = String(true);  //true matra huda boolean bhayo ani "" vitra bhayo bhane string hunxa
+// console.log(isMarried, typeof isMarried);
+
+// const userName = "Prashant"
+// Hi, I am Prashant.
+// yo dekhaune different tarika
+// console.log("Hi, I am ", userName, ".")
+// console.log("Hi, I am " + userName + ".")
+// console.log(`Hi, I am ${userName}.`)
+
+//`` use garda tyo in string hunxa
