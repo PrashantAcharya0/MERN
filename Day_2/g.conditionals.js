9f3a8f66e8bd52696a1767ed6bb550c0e35cbc92
Prashant Acharya <109913165+theblitheguy@users.noreply.github.com>
2024-06-04 21:54:40 +0545

    Add files via upload

---
+++ Day_2/g.conditionals.js
@@ -0,0 +1,163 @@
+// ? conditionals
+
+// sequential flow 
+// console.log("Hi");
+// console.log(2+3);
+// console.log("Hello");
+
+// ? if
+// ? if.. else
+// ? if ..else if ... else
+// ? switch
+// ? ternary
+
+// ? if 
+// ? syntax
+
+// console.log("Hi")
+// if(2 === 3)
+// {
+//     console.log(2 + 3);
+// }
+// console.log("Hello");
+
+// if("Nepal" === "Nepal")
+//     {
+//         console.log("Hi");     //true huda
+//     }
+//     else
+//     {
+//         console.log("Hello");  //false huda
+//     }
+
+// ? find if a number is odd or even
+// let num = 25;
+
+// let remainder = num %2;
+
+// if(remainder === 1)
+// {
+//     console.log(`${num} is odd.`)
+// }
+// else
+// {
+//     console.log(`${num} is even`)
+// }
+
+
+// ? if...else...if...else
+// let a = -25;
+// if(a < 0)
+// {
+//     console.log("-ve");
+// }
+// else if(a > 0)
+// {
+//     console.log("+ve");
+// }
+// else
+// {
+//     console.log("zero/neutral");
+// }
+
+
+// ? suppose any three positive numbers
+// ? find greatest number among them
+// let a = 1;
+// let b = 2;
+// let c = 3;
+
+// if (a<b && a<c)
+// {
+//     console.log("b greater")
+// }
+// else if (b<c)
+// {
+//     console.log("c greater");
+// }
+// else
+// {
+//     console.log("a greater ")
+// }
+
+// ? switch => best for multiple choice question
+
+// ? 1.Green
+// ? 2.Yellow
+// ? 3.Blue
+
+// ? syntax
+// switch(variable)
+// {
+//     case vale1:
+//     //code
+//     //break;}
+
+//     case vale2:
+//     //code
+//     //break;
+
+//     case valeN:
+//     //code
+//     //break;
+
+//     default:
+//     //some code 
+// }
+
+// let dayNumber = 1;
+
+// switch (dayNumber)
+// {
+//     case 1:
+//         console.log("Sunday");
+//         break;
+    
+//     case 2:
+//         console.log("Monday");
+//         break;
+    
+    
+//     case 3:
+//         console.log("Tuesday");
+//         break;
+
+    
+//     case 4:
+//         console.log("Wednesday");
+//         break;
+
+    
+//     case 5:
+//         console.log("Thursday");
+//         break;
+
+
+//     case 6:
+//         console.log("Friday");
+//         break;
+
+
+    
+//     case 7:
+//         console.log("Monday");
+//         break;
+
+
+//     default:
+//         console.log("Invalid Day Number")
+// }
+
+
+// ? ternary
+// let x = 5;
+// let remainder = x % 2;
+
+// ? condition ? expIftrue : expIffalse
+// remainder === 0 ? console.log("even"): console.log("odd");
+// console.log(remainder === 0) ? "Even" : "Odd";   //yesari lekhna ne payo
+
+// ? conditional(ternary) operator chaining example
+// let a = 0;
+
+// a<0?console.log("-ve") : a>0?console.log("+ve") : console.log("zero");
