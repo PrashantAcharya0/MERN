94d1e3948ac8167a7647545335b68ce910af50e0
Prashant Acharya <109913165+theblitheguy@users.noreply.github.com>
2024-06-09 21:24:03 +0545

    Add files via upload

---
+++ Day_5/l.object.js
@@ -0,0 +1,72 @@
+// ? object
+// ? key-value pair
+
+// const studentDetails = {};
+// console.log(studentDetails, typeof studentDetails);
+
+// const obj1 ={};
+// const obj2 ={};
+
+// console.log(obj1 === obj2); //false
+
+// let student1 = {
+//   firstName: "Prashant",
+//   lastName: "Acharya",
+//   isGraduated: true,
+//   //   permanentAddress: "Pokhara",
+//   //   temporaryAddress: "Sydney",
+//   address: {
+//     permanent: "Pokhara",
+//     temporary: "sydney",
+//   },
+// };
+// console.log(student1);
+// console.log(student1.address.permanent);
+
+//jastai My name is Prashant chaiyo
+// console.log(`My name is ${student1.firstName}`);
+
+// ? to access values in object
+// ? dot operator
+// ? square bracket operator
+
+// console,log(student1.firstname);
+// console,log(student1.permanetAddress);
+
+// console.log(student1["isGraduated"]);
+// console.log(student1["temporaryAddress"]);
+
+// ? CRUD
+// ? C => Create/Add
+// ? R => Read/Retrive
+// ? U => update/Edit
+// ? D => Delete/Remove
+
+// ? upsert or Insert
+// student1.firstName = "Razan";
+// student1.age = 27;
+// console.log(student1);
+// student1["address"]["permanent"] = "birgunji";
+// console.log(student1);
+
+// let nationDetail = {
+//   name: "Nepal",
+//   population: "3 core",
+//   perCapital: 1400,
+// };
+// console.log(nationDetail);
+
+// ? delete
+// ether "." use garne ki ta "[]" use hane
+// delete nationDetail.name;
+// delete nationDetail["name"];
+// console.log(nationDetail);
+
+const cupDetails = {
+  name: "watercup",
+  brand: "serve well",
+  color: "Pink",
+  color: "green",
+  name: "Cupcup",
+};
+console.log(cupDetails);
