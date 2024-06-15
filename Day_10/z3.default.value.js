d09a616af2df6bd78013326610b67aae14a25bde
Prashant Acharya <109913165+theblitheguy@users.noreply.github.com>
2024-06-15 13:42:28 +0545

    Add files via upload

---
+++ Day_10/z3.default.value.js
@@ -0,0 +1,33 @@
+// default value
+
+// default value haru lai parameter ma last ma dinu parxa
+// yesma getsum ko value z ma janxa.
+// const getsum = (z, x = 6, y = 0) => {
+//   let sum = x + y + z;
+
+//   return sum;
+// };
+
+// const res = getsum(10);
+// console.log(res);
+
+// const greetUser = (userName = "user") => {
+//   console.log(`Good morning ${userName}`);
+// };
+
+// greetUser();
+
+// ----------- example2 --------------
+// ? example with out default value 
+// const greetUser = (userName = "user") => {
+//   console.log(`Good morning ${userName || "user"}`); // or garko xa
+// };
+
+// greetUser("Prashant");
+
+// and
+// const greetUser = (userName = "user") => {
+//     console.log(`Good morning ${userName && "user"}`); // and garda fuifill na huda user nai print hunxa
+//   };
+  
+//   greetUser("Prashant");
