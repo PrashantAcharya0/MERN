0432df83c22608643e4f429188b9e782c8e5be96
Prashant Acharya <109913165+theblitheguy@users.noreply.github.com>
2024-06-16 16:13:30 +0545

    Add files via upload

---
+++ Day_10/z6.asynchronus.js
@@ -0,0 +1,24 @@
+// ? asynchronus
+
+// ? synchronus => existing or occuring at the same time.
+// ? asynchronus => not existing or occuring at the same time.
+
+// ? 1.node => single threaded
+// ? 2.non-blocking => does not block the code execution
+// ? 3.real world scernario of code blocking =>
+// ? for loop for billion times
+// ? image processing
+// ? Api request
+// ? db operation
+
+// U => ultra power saving
+// k => unlocked
+// H => High performance
+
+// console.log("Hi");
+
+// setTimeout(() => {
+//   console.log("Timeout");
+// }, 20000);
+
+// console.log("Hello");
