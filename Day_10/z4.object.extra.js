389aff2bd1cc49241af404256bb246fc2ce99f28
Prashant Acharya <109913165+theblitheguy@users.noreply.github.com>
2024-06-16 16:12:34 +0545

    Add files via upload

---
+++ Day_10/z4.object.extra.js
@@ -0,0 +1,20 @@
+let firstName = "Patrik";
+let lastName = "Acharya";
+let address = "Ktm";
+
+// key ra value same xa bhane repeat na gara bhanxa
+// let studentDetail = {
+//   //   firstName: firstName,
+//   //   lastName: lastName,
+//   //   address: address,
+//   firstName,
+//   lastName,
+//   address,
+// };
+// console.log(studentDetail);
+
+// let num = 5;
+// console.log(`num=${num}`); // yo garna parene
+// console.log({ num });
+
+
