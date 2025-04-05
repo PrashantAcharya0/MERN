7eb74d6f82e4bd5e70becde17665dcb96261fdf2
Prashant Acharya <acharyaprashant16@gmail.com>
2025-04-05 17:44:08 +0545

    updated

--- with-db/FOODMANDU/connect.db.js
+++ with-db/FOODMANDU/connect.db.js
@@ -1,15 +1,21 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose';
+
+const dbUserName = process.env.dbUserName;
+const dbPassword = encodeURIComponent(process.env.dbPassword);
+const dbHost = process.env.dbHost;
+const dbName = process.env.dbName;
+const dbOptions = process.env.dbOptions;
 
 const connectDB = async () => {
   try {
     await mongoose.connect(
-      `mongodb+srv://acharyaprashant16:theblitheguy12@cluster0.orjro45.mongodb.net/kec-foodmandu?retryWrites=true&w=majority&appName=Cluster0`
+      `mongodb+srv://${dbUserName}:${dbPassword}@${dbHost}/${dbName}?${dbOptions}`
     );
-    console.log("DB connection established...");
+    console.log('Connected to database');
   } catch (error) {
-    console.log("DB connection failed...");
+    console.log('Error connecting to database');
     console.log(error.message);
-    process.exit();
+    process.exit(1); //1=> exit with error
   }
 };
 
